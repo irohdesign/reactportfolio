c5677d286ec753e831fea33c7b217e236b9a42c8
Jalia Evans <irohdesign@outlook.com>
2019-02-28 10:12:56 -0500

    Updates

---
+++ precache-manifest.652ada3896988621a4409f646762f9d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58f6a6489f6893b92962",
+    "url": "/reactportfolio/static/js/runtime~main.58f6a648.js"
+  },
+  {
+    "revision": "d8540748f0988487ddaa",
+    "url": "/reactportfolio/static/js/main.d8540748.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/reactportfolio/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "d8540748f0988487ddaa",
+    "url": "/reactportfolio/static/css/main.b9ed4fb9.chunk.css"
+  },
+  {
+    "revision": "cf30a03c520b6f4fa80ced23d75342ae",
+    "url": "/reactportfolio/index.html"
+  }
+];
